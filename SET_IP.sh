c4a2c2f39157d9203e4ebf6d62ab2019d846bb4c
Tobias Weis <tobi@taurus.lan>
2012-02-22 11:40:12 +0100

    minor

--- SET_IP.sh
+++ SET_IP.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ip=`/sbin/ifconfig ppp0 | /bin/grep inet | /usr/bin/awk '{print substr($2,6)}'`
-/usr/bin/wget http://www.codalibre.de/files/beagleip.php?ip=$ip --timeout=20 --delete-after > /dev/null 2>&1
+/usr/bin/wget http://www.YOURSERVER.de/ipsite.php?ip=$ip --timeout=20 --delete-after > /dev/null 2>&1
